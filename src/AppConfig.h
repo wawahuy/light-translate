25b80e3430530681b09166ab03c0537e5a4bdd8b
wawahuy <kakahuy99@gmail.com>
2026-06-16 21:19:42 +0700

    feat: set WindowsOCR default

--- src/AppConfig.h
+++ src/AppConfig.h
@@ -24,7 +24,7 @@ struct AppConfig
 {
     // -- API ------------------------------------------------------------------
     TranslateProvider providerType = TranslateProvider::Google;
-    OcrType ocrType = OcrType::PaddleOCR; // Default to PaddleOCR
+    OcrType ocrType = OcrType::WindowsOCR; // Default to WindowsOCR
     std::wstring apiUrl = L"https://api.deepseek.com/chat/completions";
     std::wstring apiModel = L"deepseek-chat";
     std::wstring apiKey = L"";
